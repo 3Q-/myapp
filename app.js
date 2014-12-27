4642dc478ca9748f4ca9ba4f71882507146804cb
3Q <yongjy@126.com>
2014-12-27 17:26:46 +0800

    good

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+//var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
-var log = require('./lib/log');
-
-
+var log = require('./lib/log').loger('app.js');
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -21,8 +18,8 @@ app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
 // uncomment after placing your favicon in /public
-//app.use(logger('dev'));
 app.use(favicon(__dirname + '/favicon.ico'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -42,6 +39,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
+console.log(app.get('env'));
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+//var logger = require('../lib/log4js').logger('index.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
+
   res.render('index', { title: 'Express' });
 });
 
