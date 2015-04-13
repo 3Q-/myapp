fa06bad3525b2410851587434599d114e422ff06
3Q <yongjy@126.com>
2015-04-14 00:15:48 +0800

    dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,20 +5,74 @@ module.exports = function (grunt) {
     var config = {
         app: 'app',
         dist: 'dist',
-        views : 'views'
     };
     grunt.initConfig({
         config: config,
-        copy: {
-            dist:{
+        clean: {
+            dist: {
                 files: [{
-                    expand : true,
-                    cwd : '<%= config.views%>',
-                    dest : '<%= config.dist%>',
-                    src : '{,*/}*.html'
+                    dot: true,
+                    src: [
+                        'dist/*',
+                        '!dist/.git*'
+                    ]
                 }]
             }
         },
+        copy: {
+            src: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.app %>/views',
+                    src: ['*.html'],
+                    dest: '<%= config.dist %>/views'
+                },{
+                    expand: true,
+                    cwd: '<%= config.app %>/static/js',
+                    src: ['**/*.min.js'],
+                    dest: '<%= config.dist %>/static/js',
+                },{
+                    expand: true,
+                    cwd: '<%= config.app %>/static',
+                    src: ['fonts/*', '*.{ico,png.txt}'],
+                    dest: '<%= config.dist %>/static',
+                }]
+            }
+        },
+        imagemin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.app %>/static/img',
+                    src: '{,*/}*.{gif,jpeg,jpg,png}',
+                    dest: '<%= config.dist %>/static/img'
+                }]
+            }
+        },
+        uglify: {
+            prod: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.app %>/static/js',
+                    src: ['**/*.js', '!**/*.min.js'],
+                    dest: '<%= config.dist %>/static/js',
+                }]
+            }
+        },
+        cssmin: {
+            prod: {
+                options: {
+                    report: 'gzip'
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.app %>/static/css',
+                    src: ['**/*.css'],
+                    dest: '<%= config.dist %>/static/css',
+                }]
+            }
+        },
+
         // Watches files for changes and runs tasks based on the changed files
         htmlmin: {
             dist: {
@@ -36,11 +90,20 @@ module.exports = function (grunt) {
                 },
                 files: [{
                     expand: true,
-                    cwd: '<%= config.dist %>',
+                    cwd: '<%= config.dist %>/views',
                     src: '{,*/}*.html',
-                    dest: '<%= config.dist %>'
+                    dest: '<%= config.dist %>/views'
                 }]
             }
         }
     });
+
+    grunt.registerTask('default',[
+        'clean',
+        'copy',
+        'imagemin',
+        'cssmin',
+        'uglify',
+        'htmlmin'
+    ]);
 };
