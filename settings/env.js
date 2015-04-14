0db834cae687341da42747600a4bfa1b6c3557e1
3Q <yongjy@126.com>
2015-04-14 17:26:55 +0800

    evn.js modify

--- settings/env.js
+++ settings/env.js
@@ -3,26 +3,26 @@ exports.getENV = function(app){
     var env = {};
     if(app.get('env') === 'production'){
         env = {
-            js : 'http://s.bupobuli.com/static/js',
+            js : 'http://s.bupobuli.com/static/javascript',
             css : 'http://s.bupobuli.com/static/css',
-            img : 'http://s.bupobuli.com/static/img',
+            img : 'http://s.bupobuli.com/static/image',
             views : __dirname +'/../dist/views',
             static : __dirname +'/../dist/static'
         };
     }else if(app.get('env') === 'development'){
         env = {
-            js : '/static/js',
+            js : '/static/javascript',
             css : '/static/css',
-            img : '/static/img',
+            img : '/static/image',
             uploads : '/uploads/',
             views : '/projects/xiexie/app/views',
             static : '/projects/xiexie/app/static'
         };
     }else{
         env = {
-            js : '/static/js',
+            js : '/static/javascript',
             css : '/static/css',
-            img : '/static/img',
+            img : '/static/image',
             uploads : '/uploads/',
             views : __dirname +'/../dist/views',
             static : __dirname +'/../dist/static'
